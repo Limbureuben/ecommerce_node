9eab24887d7c7c65b27921cc4b7ba970ceca56d8
Limbureuben <limbureuben4@gmail.com>
2025-09-03 21:18:47 +0300

    changes

--- src/routes/product/ProductRoute.js
+++ src/routes/product/ProductRoute.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const  Product = require('../../model/product/productModel');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
-router.post('/product-register', async (req, res) => {
-    const { name, price, description } = req.body;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/product_images');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+  fileFilter: (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif|webp/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (mimetype && extname) return cb(null, true);
+    cb(new Error('Only image files are allowed!'));
+  },
+});
+
+router.post('/product-register', upload.single('image'), async (req, res) => {
+    const { name, category, price, description } = req.body;
 
     try {
+        const imageUrl = req.file
+      ? `${req.protocol}://${req.get('host')}/public/product_images/${req.file.filename}`
+      : null;
+
         const products = await Product.create({
             name,
+            category,
             price,
             description,
             image: imageUrl
@@ -26,8 +56,14 @@ router.post('/product-register', async (req, res) => {
     }
 });
 
-
-//Let us fetch the productfrom the database here
+router.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ success: false, message: err.message });
+  } else if (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
+  next();
+});
 
 //we will add a middleeware to authenticate the user
 router.get('/get-all-products', async (req, res) => {
